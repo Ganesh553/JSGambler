dd08c754972cf3665b5c9e029d64729f1335784f
Ganesh553 <Ganesh553>
2021-01-10 18:51:07 +0530

    Player Play for 20 Day

---
+++ Gambler.js
@@ -0,0 +1,30 @@
+class Gambler{
+    NumberOfdays(){
+        var day = new Map()
+        for(var i=1;i<=20;i++){
+            var value=this.play();
+            day[i] = value;
+        }
+    }
+    play(){
+        var Stake=100;
+        var day=0;
+        var count=0
+        var flag=true;
+        while(flag){
+            if(count==50){
+                flag=false;
+            }       
+            var bet=Math.floor(Math.random() * 2);
+            count=count+1;    
+            if(bet==1){
+                Stake=Stake+1;
+            }else{
+                Stake=Stake-1;
+            }
+        }
+        return Stake;
+    }     
+}
+var ref = new Gambler();
+ref.NumberOfdays();

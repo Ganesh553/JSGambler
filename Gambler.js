838ee25845aebce2eb1fbc67e7e3d18ee572648b
Ganesh553 <Ganesh553>
2021-01-11 11:46:07 +0530

    Player start playing

---
+++ Gambler.js
@@ -0,0 +1,11 @@
+class Gambler{
+    play(){
+        var Stake=100;
+        var day=0;
+        var count=0
+        var flag=true;
+        console.log("Start Playing")
+    }  
+}
+var ref = new Gambler();
+ref.play();
